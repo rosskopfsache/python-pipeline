48461a66c0063b515d31d26817003c54082be404
ceps <lukas.rosskopf@audi.de>
2019-05-29 14:12:38 +0200

    updates

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator library basic operations
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
